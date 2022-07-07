fd59147bb240e07a6e8bb227eb2b9f37b48be35c
Ethan Kisiel <ethan.a.kisiel@gmail.com>
2022-07-07 12:03:43 -0400

    Initial Commit

---
+++ UseCasesLocalRealm/UseCasesLocalRealmApp.swift
@@ -0,0 +1,17 @@
+//
+//  UseCasesLocalRealmApp.swift
+//  UseCasesLocalRealm
+//
+//  Created by Ethan Kisiel on 7/7/22.
+//
+
+import SwiftUI
+
+@main
+struct UseCasesLocalRealmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
